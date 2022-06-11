1289b7c0147c529ba98cd2b5a84f047f41040605
kahpong99 <70220@siswa.unimas.my>
2022-06-12 00:22:13 +0800

    progress

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import 'onsenui/esm/elements/ons-navigator';
 import 'onsenui/esm/elements/ons-alert-dialog';
 import 'onsenui/esm/elements/ons-alert-dialog-button';
 import pic from "../src/assets/diabetes_black.png"
+import usericon from '../src/assets/user-icon.png';
+import 'bootstrap/dist/css/bootstrap.css';
+import "../src/assets/style.css";
 
 
 import * as Ons from "react-onsenui";
@@ -63,13 +66,12 @@ export const App = () => {
             onPop={() => navigator.popPage()}  
           >
             
-            <ons-toolbar>
+            {/* <ons-toolbar>
               <div className = "center">
                 Some toolbaR
               </div>
-            </ons-toolbar>
+            </ons-toolbar> */}
 
-            <div>Some content</div>
             {/* <div style={{ textAlign: "center" }}>
               <StyledSVG />
             </div> */}
@@ -90,12 +92,25 @@ export const App = () => {
               Hello! {currentUser.name}
             </div>
             <div style={{ marginTop: "10px" }}>{JSON.stringify(user)}</div> */}
+            <div className="header-box">
+              <div className="greeting-field">
+                <div className="header__greeting">
+                  <div>Good Evening,</div>
+                  <div className="user-fullname">{user.firstName} {user.lastName}</div>
+                </div>
 
-            
-
-            <Ons.Button>
-              Button
-            </Ons.Button>
+                <img src={usericon} className="user-icon"></img>
+              </div>
+          
+            </div>
+            {console.log(JSON.stringify(user))}
+            <Ons.Row className="justify-content-md-center">
+              <Ons.Col className="d-flex align-items-center justify-content-center">
+                <Ons.Button className = "btn btn-secondary btn-sm m-2">
+                  Button
+                </Ons.Button>
+              </Ons.Col>
+            </Ons.Row>
 
             <Ons.AlertDialog>
               <div className="alert-dialog-title">Warning!</div>
@@ -113,8 +128,6 @@ export const App = () => {
             </Ons.AlertDialog>
             <ons-button>button</ons-button>
 
-
-            <img src={pic} />
           </Ons.Page>
         }
         initialRoute={{
